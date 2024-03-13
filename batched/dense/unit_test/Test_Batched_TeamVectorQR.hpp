2d0d77257b6f235f3ca0a7c358e6cef7ecbbc082
Luc Berger-Vergiat <lberge@sandia.gov>
2024-03-13 08:05:33 -0600

    Batch - QR: back-up commit

--- batched/dense/unit_test/Test_Batched_TeamVectorQR.hpp
+++ batched/dense/unit_test/Test_Batched_TeamVectorQR.hpp
@@ -109,6 +109,133 @@ struct Functor_TestBatchedTeamVectorQR {
 
 template <typename DeviceType, typename MatrixViewType, typename VectorViewType,
           typename WorkViewType, typename AlgoTagType>
+struct Functor_TestBatchedTeamVectorQR_rectangular {
+  using execution_space = typename DeviceType::execution_space;
+  MatrixViewType _a;
+  MatrixViewType _b;
+  VectorViewType _t;
+  WorkViewType _w;
+
+  KOKKOS_INLINE_FUNCTION
+  Functor_TestBatchedTeamVectorQR_rectangular(const MatrixViewType &a,
+                                              const MatrixViewType &b,
+                                              const VectorViewType &t,
+                                              const WorkViewType &w)
+      : _a(a), _b(b), _t(t), _w(w) {}
+
+  template <typename MemberType>
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType &member) const {
+    using value_type = typename MatrixViewType::non_const_value_type;
+    // const value_type one = Kokkos::ArithTraits<value_type>::one();
+    const value_type zero  = Kokkos::ArithTraits<value_type>::zero();
+    const value_type add_this(10);
+
+    const int k = member.league_rank();
+    auto aa     = Kokkos::subview(_a, k, Kokkos::ALL(), Kokkos::ALL());
+    auto bb     = Kokkos::subview(_b, k, Kokkos::ALL(), Kokkos::ALL());
+    auto tt     = Kokkos::subview(_t, k, Kokkos::ALL());
+    auto ww     = Kokkos::subview(_w, k, Kokkos::ALL());
+
+    // make diagonal dominant
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(member, aa.extent(0)),
+                         [&](const int &i) { aa(i, i) += add_this; });
+
+    /// AA = QR
+    TeamVectorQR<MemberType, AlgoTagType>::invoke(member, aa, tt, ww);
+    member.team_barrier();
+
+    // assign upper tridiagonal part to bb
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(member, aa.extent(0)),
+                         [&](const int &i) {
+                           for (int j = 0; j < aa.extent_int(1); ++j)
+                             bb(i, j) = (i <= j ? aa(i, j) : zero);
+                         });
+    member.team_barrier();
+
+    // Multiply with Q
+    TeamVectorApplyQ<MemberType, Side::Left, Trans::NoTranspose,
+                     Algo::ApplyQ::Unblocked>::invoke(member, aa, tt, bb, ww);
+    member.team_barrier();
+  }
+
+  inline void run() {
+    using value_type = typename MatrixViewType::non_const_value_type;
+    std::string name_region("KokkosBatched::Test::TeamVectorQR_rectangular");
+    const std::string name_value_type = Test::value_type_name<value_type>();
+    std::string name                  = name_region + name_value_type;
+    Kokkos::Profiling::pushRegion(name.c_str());
+
+    const int league_size = _a.extent(0);
+    Kokkos::TeamPolicy<execution_space> policy(league_size, Kokkos::AUTO);
+
+    Kokkos::parallel_for(name.c_str(), policy, *this);
+    Kokkos::Profiling::popRegion();
+  }
+};
+
+template <typename DeviceType, typename MatrixViewType, typename VectorViewType,
+          typename WorkViewType, typename AlgoTagType>
+struct Functor_TestBatchedTeamVectorQR_analytic {
+  using execution_space = typename DeviceType::execution_space;
+  MatrixViewType _a;
+  MatrixViewType _b;
+  VectorViewType _t;
+  WorkViewType _w;
+
+  KOKKOS_INLINE_FUNCTION
+  Functor_TestBatchedTeamVectorQR_analytic(const MatrixViewType &a,
+					   const MatrixViewType &b,
+					   const VectorViewType &t,
+					   const WorkViewType &w)
+    : _a(a), _b(b), _t(t), _w(w) {}
+
+  template <typename MemberType>
+  KOKKOS_INLINE_FUNCTION void operator()(const MemberType &member) const {
+    using value_type = typename MatrixViewType::non_const_value_type;
+    // const value_type one = Kokkos::ArithTraits<value_type>::one();
+    const value_type zero  = Kokkos::ArithTraits<value_type>::zero();
+
+    const int k = member.league_rank();
+    auto aa     = Kokkos::subview(_a, k, Kokkos::ALL(), Kokkos::ALL());
+    auto bb     = Kokkos::subview(_b, k, Kokkos::ALL(), Kokkos::ALL());
+    auto tt     = Kokkos::subview(_t, k, Kokkos::ALL());
+    auto ww     = Kokkos::subview(_w, k, Kokkos::ALL());
+
+    /// AA = QR
+    TeamVectorQR<MemberType, AlgoTagType>::invoke(member, aa, tt, ww);
+    member.team_barrier();
+
+    // assign upper tridiagonal part to bb
+    Kokkos::parallel_for(Kokkos::TeamVectorRange(member, aa.extent(0)),
+                         [&](const int &i) {
+                           for (int j = 0; j < aa.extent_int(1); ++j)
+                             bb(i, j) = (i <= j ? aa(i, j) : zero);
+                         });
+    member.team_barrier();
+
+    // Multiply with Q
+    TeamVectorApplyQ<MemberType, Side::Left, Trans::NoTranspose,
+                     Algo::ApplyQ::Unblocked>::invoke(member, aa, tt, bb, ww);
+    member.team_barrier();
+  }
+
+  inline void run() {
+    using value_type = typename MatrixViewType::non_const_value_type;
+    std::string name_region("KokkosBatched::Test::TeamVectorQR_rectangular");
+    const std::string name_value_type = Test::value_type_name<value_type>();
+    std::string name                  = name_region + name_value_type;
+    Kokkos::Profiling::pushRegion(name.c_str());
+
+    const int league_size = _a.extent(0);
+    Kokkos::TeamPolicy<execution_space> policy(league_size, Kokkos::AUTO);
+
+    Kokkos::parallel_for(name.c_str(), policy, *this);
+    Kokkos::Profiling::popRegion();
+  }
+};
+
+template <typename DeviceType, typename MatrixViewType, typename VectorViewType,
+          typename WorkViewType, typename AlgoTagType>
 void impl_test_batched_qr(const int N, const int BlkSize) {
   typedef typename MatrixViewType::non_const_value_type value_type;
   typedef Kokkos::ArithTraits<value_type> ats;
@@ -151,6 +278,117 @@ void impl_test_batched_qr(const int N, const int BlkSize) {
     }
   }
 }
+
+template <typename DeviceType, typename MatrixViewType, typename VectorViewType,
+          typename WorkViewType, typename AlgoTagType>
+void impl_test_batched_qr_rectangular(const int N, const int BlkSize1,
+                                      const int BlkSize2) {
+  typedef typename MatrixViewType::non_const_value_type value_type;
+  typedef Kokkos::ArithTraits<value_type> ats;
+  // const value_type one = Kokkos::ArithTraits<value_type>::one();
+
+  /// randomized input testing views
+  MatrixViewType a("a", N, BlkSize1, BlkSize2);
+  MatrixViewType b("b", N, BlkSize1, BlkSize2);
+  VectorViewType t("t", N, BlkSize2);
+  WorkViewType w("w", N, 20 * BlkSize1);
+
+  Kokkos::fence();
+
+  Kokkos::Random_XorShift64_Pool<typename DeviceType::execution_space> random(
+      13718);
+  Kokkos::fill_random(a, random, value_type(1.0));
+
+  Kokkos::fence();
+
+  Functor_TestBatchedTeamVectorQR_rectangular<
+      DeviceType, MatrixViewType, VectorViewType, WorkViewType, AlgoTagType>(
+      a, b, t, w)
+      .run();
+
+  Kokkos::fence();
+
+  /// for comparison send it to host
+  typename MatrixViewType::HostMirror a_host = Kokkos::create_mirror_view(a);
+  Kokkos::deep_copy(a_host, a);
+  typename MatrixViewType::HostMirror b_host = Kokkos::create_mirror_view(b);
+  Kokkos::deep_copy(b_host, b);
+
+  /// check x = 1; this eps is about 1e-14
+  typedef typename ats::mag_type mag_type;
+  const mag_type eps = 1e3 * ats::epsilon();
+
+  for (int k = 0; k < N; ++k) {
+    for (int i = 0; i < BlkSize1; ++i) {
+      for (int j = 0; j < BlkSize2; ++j) {
+        const mag_type diff = ats::abs(a_host(k, i, j) - b_host(k, i, j));
+        printf("k = %d, i = %d, j= %d, a %e b %e diff %e \n", k, i, j,
+               a_host(k, i, j), b_host(k, i, j), diff);
+        EXPECT_NEAR_KK(diff, mag_type(0), eps);
+      }
+    }
+  }
+}
+
+template <typename DeviceType, typename MatrixViewType, typename VectorViewType,
+          typename WorkViewType, typename AlgoTagType>
+void impl_test_batched_qr_analytic(const int N) {
+  // A = [12  -51    4]
+  //     [ 6  157  -68]
+  //     [-4   24  -41]
+  //
+  // Q = [ 6/7  -69/175  58/175]
+  //     [ 3/7  158/175  -6/175]
+  //     [-2/7    6/25   33/35 ]
+  //
+  // R = [14   21  -14]
+  //     [ 0  175  -70]
+  //     [ 0    0  -35]
+  //
+  // v0 = 1/sqrt(14)*[-1]  ==> u0 = [ 1]
+  //                 [ 3]           [-3]
+  //                 [-2]           [ 2]
+  //
+  // v1 = 1/280*[-224]     ==> u1 = [1]
+  //            [ 168]              [-168/224]
+
+  /// randomized input testing views
+  MatrixViewType a("a", N, 3, 3);
+  MatrixViewType b("b", N, 3, 3);
+  VectorViewType t("t", N, 3);
+  WorkViewType w("w", N, 20 * 3);
+
+  Kokkos::fence();
+
+  typename MatrixViewType::HostMirror a_h = Kokkos::create_mirror_view(a);
+  for(int matIdx = 0; matIdx < N; ++matIdx) {
+    a_h(matIdx, 0, 0) = 12; a_h(matIdx, 0, 1) = -51; a_h(matIdx, 0, 2) =   4;
+    a_h(matIdx, 1, 0) =  6; a_h(matIdx, 1, 1) = 167; a_h(matIdx, 1, 2) = -68;
+    a_h(matIdx, 2, 0) = -4; a_h(matIdx, 2, 1) =  24; a_h(matIdx, 2, 2) = -41;
+  }
+  Kokkos::deep_copy(a, a_h);
+
+  Kokkos::fence();
+
+  Functor_TestBatchedTeamVectorQR_analytic<
+      DeviceType, MatrixViewType, VectorViewType, WorkViewType, AlgoTagType>(
+      a, b, t, w)
+      .run();
+
+  Kokkos::fence();
+
+  /// for comparison send it to host
+  typename VectorViewType::HostMirror t_h = Kokkos::create_mirror_view(t);
+  Kokkos::deep_copy(t_h, t);
+  Kokkos::deep_copy(a_h, a);
+
+  std::cout << "a = [" << a_h(0, 0, 0) << ", " << a_h(0, 0, 1) << ", " << a_h(0, 0, 2) << "]\n"
+	    << "    [" << a_h(0, 1, 0) << ", " << a_h(0, 1, 1) << ", " << a_h(0, 1, 2) << "]\n"
+	    << "    [" << a_h(0, 2, 0) << ", " << a_h(0, 2, 1) << ", " << a_h(0, 2, 2) << "]" << std::endl;
+
+  std::cout << "t = [" << t_h(0, 0) << ", " << t_h(0, 1) << ", " << t_h(0, 2) << "]" << std::endl;
+}
+
 }  // namespace Test
 
 template <typename DeviceType, typename ValueType, typename AlgoTagType>
@@ -165,11 +403,19 @@ int test_batched_qr() {
         WorkViewType;
     Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
                                WorkViewType, AlgoTagType>(0, 10);
-    for (int i = 1; i < 10; ++i) {
-      // printf("Testing: LayoutLeft,  Blksize %d\n", i);
-      Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
-                                 WorkViewType, AlgoTagType>(1024, i);
-    }
+    // for (int i = 1; i < 10; ++i) {
+    //   // printf("Testing: LayoutLeft,  Blksize %d\n", i);
+    //   printf("Testing: LayoutLeft,  Blksize %d\n", i);
+    //   Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
+    //                              WorkViewType, AlgoTagType>(1024, i);
+    //   printf("Testing: LayoutLeft,  Blksize %d %d\n", 2 * i, i);
+    //   Test::impl_test_batched_qr_rectangular<DeviceType, MatrixViewType,
+    // 					     VectorViewType, WorkViewType,
+    // 					     AlgoTagType>(1024, i, i);
+    // }
+    Test::impl_test_batched_qr_analytic<DeviceType, MatrixViewType,
+					VectorViewType, WorkViewType,
+					AlgoTagType>(1);
   }
 #endif
 #if defined(KOKKOSKERNELS_INST_LAYOUTRIGHT)
@@ -182,11 +428,18 @@ int test_batched_qr() {
         WorkViewType;
     Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
                                WorkViewType, AlgoTagType>(0, 10);
-    for (int i = 1; i < 10; ++i) {
-      // printf("Testing: LayoutRight, Blksize %d\n", i);
-      Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
-                                 WorkViewType, AlgoTagType>(1024, i);
-    }
+    // for (int i = 1; i < 10; ++i) {
+    //   // printf("Testing: LayoutRight, Blksize %d\n", i);
+    //   printf("Testing: LayoutRight, Blksize %d\n", i);
+    //   Test::impl_test_batched_qr<DeviceType, MatrixViewType, VectorViewType,
+    //                              WorkViewType, AlgoTagType>(1024, i);
+    //   Test::impl_test_batched_qr_rectangular<DeviceType, MatrixViewType,
+    //                                          VectorViewType, WorkViewType,
+    //                                          AlgoTagType>(1024, i, i);
+    // }
+    Test::impl_test_batched_qr_analytic<DeviceType, MatrixViewType,
+					VectorViewType, WorkViewType,
+					AlgoTagType>(1);
   }
 #endif
 
